302ef62c6714bb83729a0f2b8fc6cd01fa0a50ca
yogeshchaudhary1433 <148415369+yogeshchaudhary1433@users.noreply.github.com>
2023-10-20 12:32:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_10_22172012041/Person.kt
@@ -0,0 +1,7 @@
+package com.example.mad_practical_10_22172012041
+
+import java.io.Serializable
+
+class Person(var id:String,var name:String,var emailid:String,var phoneno:String,var address:String,var latitude:Double,var longitude:Double):Serializable {
+
+}
